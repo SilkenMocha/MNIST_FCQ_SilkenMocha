948a65b078dbd9c209d3433d07fd493f1fb29769
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-10-20 08:39:23 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+
+st.title("Mi aplicación")
+st.sidebar.header("Esta aplicación es solo un demo")
+st.sidebar.image("http://fcq.uach.mx/images/institucionales/Escudos/Dorado.png")
+
+boton = st.button("Globos")
+if boton = true:
+  st.balloons()
