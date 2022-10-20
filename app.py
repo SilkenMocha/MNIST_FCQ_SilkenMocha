48f8fd7e3ca176804576b5aeed2a15fff19159c6
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-10-20 08:48:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title("Mi aplicación")
 st.sidebar.header("Esta aplicación es solo un demo")
-st.sidebar.image("http://fcq.uach.mx/images/institucionales/Escudos/Blanco-y-negro.png")
+st.sidebar.image("http://fcq.uach.mx/images/notas/IQ/IQ.jpg")
 
 boton = st.button("Globos")
 if boton:
-  st.balloons()
+  st.balloons():
