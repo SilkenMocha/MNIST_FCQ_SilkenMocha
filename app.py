03b0b4af66cb6e15e7dea83c96c6c6dab3b750e8
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-10-20 08:45:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Mi aplicación")
 st.sidebar.header("Esta aplicación es solo un demo")
-st.sidebar.image("http://fcq.uach.mx/images/institucionales/Escudos/Dorado.png")
+st.sidebar.image("http://fcq.uach.mx/images/institucionales/Escudos/Blanco-y-negro.png")
 
 boton = st.button("Globos")
 if boton = true:
