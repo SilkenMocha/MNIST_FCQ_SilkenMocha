6d4aaf88b1c2534919dddb3fb58a47527f151289
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-10-20 08:46:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.sidebar.header("Esta aplicación es solo un demo")
 st.sidebar.image("http://fcq.uach.mx/images/institucionales/Escudos/Blanco-y-negro.png")
 
 boton = st.button("Globos")
-if boton = true:
+if boton == true:
   st.balloons()
